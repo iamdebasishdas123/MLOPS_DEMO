7b5922a8164b8af4e8042b18a0c6656ec1c47283
Debasish Das <iamdebasishdas123@gmail.com>
2026-04-18 14:52:41 +0530

    minor bug debuging

--- app.py
+++ app.py
@@ -10,13 +10,8 @@ def mul(a, b):
     return a*b
 
 
-def div(a, b):
-    return a/b
-
-
 if __name__ == "__main__":
     result1 = add(4, 5)
     result2 = sub(4, 1)
 
     print(result1, result2)
-    
