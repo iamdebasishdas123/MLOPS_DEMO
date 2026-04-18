6143a2ffd37218fdb1adc0994383c71806ff136f
Debasish Das <iamdebasishdas123@gmail.com>
2026-04-18 14:44:55 +0530

    add testing and linting module

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def sub(a, b):
+    return a-b
 
 
+def mul(a, b):
+    return a*b
 
-def sub(a,b):return a-b
 
-result1 = add(4,5)
-result2 = sub(4,1)
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(4, 1)
 
-print(result1,result2)
+    print(result1, result2)
